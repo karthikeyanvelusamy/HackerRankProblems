e6a8c205f375f0901d0c38c57766af43ab022b06
karthikeyan.v <karthikeyan.vs@appviewx.com>
2016-09-08 17:22:09 +0530

    Icecreamparler problem commit

--- src/searching/IceCreamParlerProblem.java
+++ src/searching/IceCreamParlerProblem.java
@@ -1,6 +1,8 @@
 package searching;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class IceCreamParlerProblem {
 
@@ -12,10 +14,27 @@ public class IceCreamParlerProblem {
 		while (iTestCase-- > 0) {
 			int m = Integer.parseInt(reader.readLine());
 			int n = Integer.parseInt(reader.readLine());
-			
+			boolean flag = false;
 			String[] data = reader.readLine().split("\\ ");
-					
-					
+			int[] values = new int[n];
+			int i = 0;
+			for (String number : data) {
+				values[i++] = Integer.parseInt(number);
+			}
+			int j = 0;
+			for (i = 0; i < n - 1; i++) {
+				int t = m - values[i];
+				for ( j = i + 1; j < n; j++) {
+					if (values[j] == t) {
+						flag = true;
+						break;
+					}
+				}
+				if (flag)
+					break;
+			}
+			if (flag)
+				System.out.println((i + 1) + " " + (j + 1));
 		}
 	}
 
